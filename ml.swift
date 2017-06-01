2c769a48cc8d3b0dc26b43fe1c019e71b3d60e71
Deepak <deepak@deepakmac.local>
2017-06-01 11:22:59 +0530

    Initial commit

---
+++ ml.swift
@@ -0,0 +1,35 @@
+//
+//  ml.swift
+//  ml
+//
+//  Created by Deepak on 31/05/2016 A.
+//  Copyright © 2017 Adyog. All rights reserved.
+//
+
+import Foundation
+
+
+public class ml{
+    
+    private var isDebug: Bool!
+    
+    //2.
+    public init() {
+        self.isDebug = false
+    }
+    
+    //3.
+    public func setup(isDebug: Bool) {
+        self.isDebug = isDebug
+        print("Project is in Debug mode: \(isDebug)")
+    }
+    
+    //4.
+    public func YPrint<T>(value: T) {
+        if self.isDebug == true {
+            print(value)
+        } else {
+            //Do any stuff for production mode
+        }
+    }
+}
---
+++ ml/ml.h
@@ -0,0 +1,19 @@
+//
+//  ml.h
+//  ml
+//
+//  Created by Deepak on 31/05/2016 A.
+//  Copyright © 2017 Adyog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ml.
+FOUNDATION_EXPORT double mlVersionNumber;
+
+//! Project version string for ml.
+FOUNDATION_EXPORT const unsigned char mlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ml/PublicHeader.h>
+
+
